04a65cb3ba28bc924409764a9ebf40ed2d991ede
Harsh Kashiwal <77677724+KashiwalHarsh@users.noreply.github.com>
2022-07-26 16:53:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulb_on() {
+    document.getElementById('bulb').src = 'bulb-images/bulb-3-on.png';
+}
+function bulb_off() {
+    document.getElementById('bulb').src = 'bulb-images/bulb-3-off.png';
+}
